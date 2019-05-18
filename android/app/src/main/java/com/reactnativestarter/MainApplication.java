04ade66e3539986c7448be6b6461e4a80848f818
Ding <ding.ling@buzznog.com>
2019-05-19 00:38:47 +0800

    configure android project

--- android/app/src/main/java/com/reactnativestarter/MainApplication.java
+++ android/app/src/main/java/com/reactnativestarter/MainApplication.java
@@ -17,6 +17,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
             new RNCWebViewPackage(),
             new Sha256Package(),
             new ReactNativeConfigPackage(),
